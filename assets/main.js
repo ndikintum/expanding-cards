007e698ebf6899ffde6e70266f851dd335a835c3
HARD PRO <hardpro@HARDs-MacBook-Pro.local>
2024-01-08 01:02:02 +0100

    linters

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,13 @@
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
-  panel.addEventListener('click' , () => {
+  panel.addEventListener('click', () => {
     removeActiveClasses()
-     panel.classList.add('active')
+    panel.classList.add('active')
   })
 })
 
-function  removeActiveClasses () {
+function removeActiveClasses () {
   panels.forEach((panel) => {
     panel.classList.remove('active')
   })
